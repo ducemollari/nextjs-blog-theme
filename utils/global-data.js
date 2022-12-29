af15709fbcdc5d763b6f4b56eb891fbc34b889fc
ducemollari <45874555+ducemollari@users.noreply.github.com>
2022-12-29 14:07:49 +0300

    2

--- utils/global-data.js
+++ utils/global-data.js
@@ -1,10 +1,10 @@
 export const getGlobalData = () => {
   const name = process.env.BLOG_NAME
     ? decodeURI(process.env.BLOG_NAME)
-    : 'Сергей';
+    : 'Sergey';
   const blogTitle = process.env.BLOG_TITLE
     ? decodeURI(process.env.BLOG_TITLE)
-    : 'Люблю помогать';
+    : 'Like to help others';
   const footerText = process.env.BLOG_FOOTER_TEXT
     ? decodeURI(process.env.BLOG_FOOTER_TEXT)
     : 'All rights reserved.';
